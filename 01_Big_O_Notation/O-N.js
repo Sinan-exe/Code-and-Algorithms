8282a430a2e058ea4d9b34c37816115922aab011
Ismail Sinan <ismailsinan2003@gmail.com>
2026-03-06 12:11:39 +0400

    learned O(N) Linear Time Complexity

---
+++ 01_Big_O_Notation/O-N.js
@@ -0,0 +1,9 @@
+function printItems(n) {
+  for (let i = 0; i < n; i++) {
+    console.log(i);
+  }
+}
+
+printItems(5);
+
+// In this function the operation is completely dependent on the input value as the input value increases the number of opeartion increases sp it is called as Linear Time complexity O(N) as n increases the complexity increases.
