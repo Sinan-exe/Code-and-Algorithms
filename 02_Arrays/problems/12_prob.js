d04ca19dd8f775f4465ebecf7a174c67df24aa0e
Ismail Sinan <ismailsinan2003@gmail.com>
2026-03-10 11:30:17 +0400

    solved array problem

---
+++ 02_Arrays/problems/12_prob.js
@@ -0,0 +1,24 @@
+// Leetcode 125 valid Palindrome
+
+function isPalindrome(s) {
+  let left = 0;
+  let right = s.length - 1;
+
+  while (left < right) {
+    while (left < right && !/[a-z0-9]/i.test(s[left])) {
+      left++;
+    }
+    while (left < right && !/[a-z0-9]/i.test(s[right])) {
+      right--;
+    }
+
+    if (s[left].toLowerCase() !== s[right].toLowerCase()) {
+      return false;
+    }
+    left++;
+    right--;
+  }
+  return true;
+}
+
+console.log(isPalindrome("A man, a plan, a canal: Panama"));
