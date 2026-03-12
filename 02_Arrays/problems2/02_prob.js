75b95fa3dc9899b91f981a33e63cc0060c9e71d1
Ismail Sinan <ismailsinan2003@gmail.com>
2026-03-12 21:15:32 +0400

    solved multiple Pointer problem

---
+++ 02_Arrays/problems2/02_prob.js
@@ -0,0 +1,19 @@
+// Multiple Pointer pattern
+
+function sumZero(nums) {
+  let left = 0;
+  let right = nums.length - 1;
+
+  while (left < right) {
+    let sum = nums[left] + nums[right];
+    if (sum === 0) {
+      return [nums[left], nums[right]];
+    } else if (sum > 0) {
+      right--;
+    } else {
+      left++;
+    }
+  }
+}
+
+console.log(sumZero([-3, -2, -1, 0, 1, 2, 3]));
