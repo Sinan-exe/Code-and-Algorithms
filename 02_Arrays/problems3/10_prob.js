ae93d501adb256ce86564b800f6979c4563e2284
Ismail Sinan <ismailsinan2003@gmail.com>
2026-03-22 16:21:09 +0400

    solved Array Problem

---
+++ 02_Arrays/problems3/10_prob.js
@@ -0,0 +1,16 @@
+function smallerNumber(nums) {
+  let arr = [];
+  let count = 0;
+  for (let i = 0; i < nums.length; i++) {
+    for (let j = 0; j < nums.length; j++) {
+      if (nums[i] > nums[j]) {
+        count++;
+      }
+    }
+    arr.push(count);
+    count = 0;
+  }
+  return arr;
+}
+
+console.log(smallerNumber([7, 7, 7, 7]));
